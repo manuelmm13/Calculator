ba29328884244ee3aba27dad227eabb64fe9ed87
manuelmm13 <54316883+manuelmm13@users.noreply.github.com>
2020-01-08 16:04:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// function to feed tag value into the screen element
 
+function showDigit(){
+    let screen = document.getElementsByClassName(screen)
+    let y = document.getElementsByTagName("button").value
+
+    screen = screen.setAttribute("value", y)
+
+    return screen
+}
