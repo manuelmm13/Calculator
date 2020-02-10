c4267bcc081f50876612ed0d8fb2dd1b56d5150c
Torok Gabor <gabor@20y.hu>
2020-02-10 11:32:06 +0100

    Introduce a React-like state and separate update and render functions

--- script.js
+++ script.js
@@ -1,64 +1,62 @@
-let Memory = null;
-let Memory2 = null;
-let sumClicked = false;
 
-
-function clickSum () {
-    sumClicked = true;
+function handleClickSum (state) {
+    state.sumClicked = true;
 }
 
-function handleClicks(){
+function init(state){
     const buttonsPanel = document.querySelector("#calculator-buttons");
     buttonsPanel.addEventListener("click", function(event){
         const btn = event.target;
         if (isNaN(btn.value)) { return false; }
-        Action(btn.value);
+        updateMemory(state, btn.value);
+        renderScreen(state)
     });
 
     let sumbutton = document.getElementsByClassName("btn")[10]
-    sumbutton.addEventListener("click", function(){clickSum()})
+    sumbutton.addEventListener("click", () => handleClickSum(state))
 
     let acbutton = document.getElementsByClassName("btn")[14]
-    acbutton.addEventListener("click", function(){clearScreen()})
+    acbutton.addEventListener("click", function(){
+        resetMemory(state);
+        renderScreen(state)
+    })
+
+    renderScreen(state)
 }
 
-function clearScreen() {
+function resetMemory(state) {
+    state.Memory = null
+    state.Memory2 = null
+}
+
+function renderScreen (state) {
     let screen = document.getElementById("screen")
-    screen.value = 0
-    Memory = null
-    Memory2 = null
-}
-
-function printToScreen (number) {
-    let screen = document.getElementById("screen")
-    screen.value = number
-}
-
-
-function Action (value) {
-
-    if (sumClicked == false) {
-        if (Memory == null) {
-            Memory = value
-        } else {
-            Memory = Memory + value
-            console.log(Memory)
-        } 
-        printToScreen(Memory)
+    if (state.sumClicked == false) {
+        screen.value = state.Memory
     } else {
-        if (Memory2 == null) {
-            Memory2 = value
-        } else {
-            Memory2 = Memory2 + value
-            console.log(Memory2)
+        screen.value = state.Memory2
     }
-    printToScreen(Memory2)
-}
-   
-    
-    
-    console.log(Memory)
 }
 
-handleClicks();
 
+function updateMemory (state, value) {
+    if (state.sumClicked == false) {
+        if (state.Memory == null) {
+            state.Memory = value
+        } else {
+            state.Memory = state.Memory + value
+        } 
+    } else {
+        if (state.Memory2 == null) {
+            state.Memory2 = value
+        } else {
+            state.Memory2 = state.Memory2 + value
+        }
+    }
+}
+
+init({
+    Memory: null,
+    Memory2: null,
+    sumClicked: false
+});
